0a6c4c1cf81a210af6d468d7396b11bf8afe3e9c
Stevenrej <steveo732@gmail.com>
2023-03-10 16:14:54 -0600

    routes to all the pages set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
 
 import './App.css'
-import Splash from './components/splash'
-import Header from './components/header'
+import { Route, Routes, Navigate } from 'react-router-dom';
+
+import About from './components/about'
+import Contact from './components/contact'
+import Gallery from './components/gallery'
 import Footer from './components/footer'
+import Header from './components/header'
+import Repairs from './components/repairs'
+import Splash from './components/splash'
 
 function App() {
 
   return (
     <div className="App">
       <Header/>
-      <Splash/>
+      <Routes>
+        <Route path="/" element={<Splash />} />
+        <Route path="/repairs" element={<Repairs />} />
+        <Route path="/gallery" element={<Gallery/>} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
       <Footer/>
     </div>
   )
