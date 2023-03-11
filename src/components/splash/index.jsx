d76b35265b64ee8b247192eb8cba31662bcafef3
Stevenrej <steveo732@gmail.com>
2023-03-10 18:22:02 -0600

    content added to splash page

--- src/components/splash/index.jsx
+++ src/components/splash/index.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import React from 'react';
+import { Button } from '@mantine/core';
+import { useDispatch } from 'react-redux';
+import { setActive } from '../../store/reducers/activeSlice';
+import { useNavigate } from 'react-router-dom';
 
 export default function Splash() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleNavClick = (value) => {
+    dispatch(setActive(value));
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+    navigate('/' + value);
+  };
+
   return (
-    <div>Splash</div>
-  )
+    <div className='splash'>
+      <div className='splash__image'>
+          <Button
+            className='splash__button'
+            color='orange'
+            size='xl'
+            uppercase
+            onClick={() => handleNavClick('repairs')}
+          >
+            Get Fixed Up Now!
+          </Button>
+      </div>
+    </div>
+  );
 }
