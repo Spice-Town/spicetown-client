4eb668f373861cd240b757e2d4ba8ea3d424fa89
Stevenrej <steveo732@gmail.com>
2023-03-11 12:24:49 -0600

    content for contact page added

--- src/components/contact/index.jsx
+++ src/components/contact/index.jsx
@@ -1,7 +1,46 @@
 import React from 'react'
+import { Button, Input, Textarea } from '@mantine/core';
 
 export default function Contact() {
   return (
-    <div>Contact</div>
+    <div className='contact'>
+      <h2>Contact Form</h2>
+      <Input.Wrapper
+        label="Your Name"
+        required
+        className='contact__input'
+        >
+        <Input
+          placeholder="Name"
+        />
+      </Input.Wrapper>
+      <Input.Wrapper
+        label="Your Phone Number"
+        required
+        className='contact__input'
+      >
+        <Input
+          placeholder="(000)-000-0000"
+          mask="(000) 000-0000"
+        />
+      </Input.Wrapper>
+      <Textarea
+        placeholder="Setup needed, replace tuners and strings."
+        label="Description of Issue"
+        description="In as much detail as possible describe the problem with your instrument"
+        radius="md"
+        withAsterisk
+        className='contact__input'
+      />
+      <Button
+        color='orange'
+        size='lg'
+        uppercase
+      // onClick={() => handleNavClick('')}
+        className='contact__button'
+      >
+        Submit
+      </Button>
+    </div>
   )
 }
