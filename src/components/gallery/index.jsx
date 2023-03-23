35236c1df3cb8ca713f2287769106e6f26590b25
Stevenrej <steveo732@gmail.com>
2023-03-23 16:09:06 -0500

    gallery updated for mobile

--- src/components/gallery/index.jsx
+++ src/components/gallery/index.jsx
@@ -8,6 +8,10 @@ export default function Gallery({ loggedIn }) {
   const [loading, setLoading] = useState(null);
   const [error, setError] = useState(false);
 
+  const isMobile = window.matchMedia('(max-width: 924px)').matches;
+
+  console.log(isMobile)
+
   // `${import.meta.env.VITE_SERVER}/item`
 
   // http://localhost:3001/item 
@@ -52,18 +56,32 @@ export default function Gallery({ loggedIn }) {
       <div className='gallery__container'>
         {photos.slice().reverse().map(photo => (
           <div className='gallery__image-container' key={photo._id}>
-            <div className='gallery__image' >
-              <Image
-                height={1000}
-                width={700}
-                radius='xs'
-                onClick={() => {
-                  setSelectedPic(photo);
-                  setOpened(true);
-                }}
-                src={photo.url}
-                alt={photo._id}
-              />
+            <div className='gallery__image'>
+              {isMobile ? (
+                <Image
+                  height={600}
+                  width={300}
+                  radius='xs'
+                  onClick={() => {
+                    setSelectedPic(photo);
+                    setOpened(true);
+                  }}
+                  src={photo.url}
+                  alt={photo._id}
+                />
+              ) : (
+                <Image
+                  height={1000}
+                  width={700}
+                  radius='xs'
+                  onClick={() => {
+                    setSelectedPic(photo);
+                    setOpened(true);
+                  }}
+                  src={photo.url}
+                  alt={photo._id}
+                />
+              )}
             </div>
             <p className='gallery__title'>
               {photo.title}</p>
@@ -83,25 +101,25 @@ export default function Gallery({ loggedIn }) {
           onClose={() => setOpened(false)}
         >
           <div className='gallery__modal-content'>
-          {selectedPic && (
-            <>
-              <div className='gallery__modal-image'>
-              <Image
-              height={700}
-              width={600}
-                src={selectedPic.url}
-                alt={selectedPic._id}
-              />
-              </div>
-              <div className='gallery__modal-text-area'>
-                <p className='gallery__modal-title'>{selectedPic.title}</p>
-                <p className='gallery__modal-dim'>{selectedPic.date}</p>
-                <div className='gallery__modal-border'>
-                <p className='gallery__modal-text'>{selectedPic.description}</p>
+            {selectedPic && (
+              <>
+                <div className='gallery__modal-image'>
+                  <Image
+                    height={700}
+                    width={600}
+                    src={selectedPic.url}
+                    alt={selectedPic._id}
+                  />
                 </div>
-              </div>
-            </>
-          )}
+                <div className='gallery__modal-text-area'>
+                  <p className='gallery__modal-title'>{selectedPic.title}</p>
+                  <p className='gallery__modal-dim'>{selectedPic.date}</p>
+                  <div className='gallery__modal-border'>
+                    <p className='gallery__modal-text'>{selectedPic.description}</p>
+                  </div>
+                </div>
+              </>
+            )}
           </div>
         </Modal>
       </div>
